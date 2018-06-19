f384378cd831041ea3593471e8d7ad3f2f7939e9
Bruno Goncalves <bugotech@gmail.com>
2018-06-19 08:50:39 -0300

    add contract exception

---
+++ src/Contracts/Exception/ExceptionHandler.php
@@ -0,0 +1,32 @@
+<?php namespace Nano7\Foundation\Contracts\Exception;
+
+use Exception;
+
+interface ExceptionHandler
+{
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function report(Exception $e);
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Nano7\Http\Request  $request
+     * @param  \Exception  $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Exception $e);
+
+    /**
+     * Render an exception to the console.
+     *
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function renderForConsole($output, Exception $e);
+}

2826d78bc73c8fdfb35fa46a6cccef88a10572c0
Bruno Goncalves <bugotech@gmail.com>
2018-06-20 10:32:09 -0300

    add in

--- src/Support/Arr.php
+++ src/Support/Arr.php
@@ -2,5 +2,29 @@
 
 class Arr extends \Illuminate\Support\Arr
 {
+    /**
+     * Determine whether the given value is array accessible.
+     *
+     * @param  array $array
+     * @param  array|mixed $find
+     * @return bool
+     */
+    public static function in($array, $find, $all = true)
+    {
+        $find = (array) $find;
 
+        foreach ($find as $item) {
+            if (in_array($item, $array)) {
+                if (! $all) {
+                    return true;
+                }
+            } else {
+                if ($all) {
+                    return false;
+                }
+            }
+        }
+
+        return $all;
+    }
 }

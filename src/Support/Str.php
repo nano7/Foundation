5bbcd9fd3f76743b990f7e27d0db59087b644df3
Bruno Goncalves <bugotech@gmail.com>
2018-08-13 15:48:17 -0300

    unstudly

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -3,6 +3,26 @@
 class Str extends \Illuminate\Support\Str
 {
     /**
+     * Convert a value to unstudly caps case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function unstudly($value)
+    {
+        $rets = [];
+        $value = ucfirst($value);
+
+        preg_match_all('/([A-Z]{1}[a-z0-9_]+)/', $value, $items, PREG_PATTERN_ORDER);
+        for ($i = 0; $i < count($items[0]); $i++) {
+            $item = $items[1][$i];
+            $rets[] = strtolower($item);
+        }
+
+        return $rets;
+    }
+
+    /**
      * @param $value
      * @return bool|null|string
      */
